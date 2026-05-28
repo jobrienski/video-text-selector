6bdd0e5a813ba7eeba6cba462063765676e13172
Jim O'Brien <jim.obrien@originate.com>
2026-05-27 22:06:37 -0700

    Release v0.2.0

--- src/manifest.ts
+++ src/manifest.ts
@@ -3,7 +3,7 @@ import { defineManifest } from "@crxjs/vite-plugin";
 export default defineManifest({
   manifest_version: 3,
   name: "Video Text Selector",
-  version: "0.1.0",
+  version: "0.2.0",
   description:
     "Cmd/Ctrl-click text inside any playing video to make it selectable and copyable, just like text on a webpage.",
   homepage_url: "https://github.com/jobrienski/video-text-selector",
